d9c3d58c85f049c5032df3a0805495d678819896
John Jeong <jjohnvi@gmail.com>
2022-10-13 17:19:15 -0500

    givebacks api started with base url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,42 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface resData {
+  causes: Array<{
+    name: string;
+    zip_code: number;
+    city: string;
+    country: string;
+    state: string;
+  }>;
+}
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>;
+  const [resultData, setResultData] = useState<resData>();
+
+  const getName = async () => {
+    const result = await axios.get(
+      `https://api.givebacks.com/services/core/causes/search[sugar][1]`
+    );
+  };
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      const result = await axios.get(
+        `https://api.givebacks.com/services/core/causes/search`
+      );
+      setResultData(result.data);
+    };
+    fetchResult();
+  }, []);
+  console.log(resultData);
+
+  const displayCauses = resultData?.causes.map((cause, i) => {
+    return <div key={i}>{cause.name}</div>;
+  });
+
+  return <div>{displayCauses}</div>;
 };
 
 export default Home;
